2748ea68ba60f67f216f48484bc8e5389bc6fbf6
Kacper Kieplin <116282757+KortezGreen@users.noreply.github.com>
2025-03-28 14:34:50 +0100

    Update wskazniki.c

--- wskazniki.c
+++ wskazniki.c
@@ -4,29 +4,39 @@
 int main()
 {
 //zad 3.2.1
-//int a = 1;
-//int b = 2;
+//int a = 1, b = 2;
 //printf("Odpowiedz to: %d", zwrot(a,b));
 
 //zad 3.2.2
+//int a = 1, b = 2;
+//printf("Odpowiedz to: %d", zwrot2(&a,&b));
 
-int a = 1;
-int b = 2;
-printf("Odpowiedz to: %d", zwrot2(a,b));
+//zad 3.2.3
+//int a = 1, b = 2;
+//printf("Przed zamiana: x = %d, y = %d\n", a, b);
+//zwrot3(&a, &b);
+//printf("Po zamianie: x = %d, y = %d\n", a, b);
 
 
 return 0;
 }
 
+int zwrot3(int *a,int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 int zwrot2(int *a,int *b)
 {
-if(a < b)
+if(*a < *b)
 {
-return a;
+return *a;
 }
 else
 {
-return b;
+return *b;
 }
 }
 
